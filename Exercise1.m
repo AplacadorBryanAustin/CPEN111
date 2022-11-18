6f52bb265e3015c8e095096d35b4d3d5283e0887
Bryan Aplacador <115542131+BryanAplacador@users.noreply.github.com>
2022-11-18 19:12:30 +0800

    Add files via upload

---
+++ Exercise1.m
@@ -0,0 +1,27 @@
+%Aplacador, Bryan Austin D.
+%BSCpE 4-2
+%CPEN111
+%Exercise1
+
+clc;
+clf;
+clear all;
+close all;
+pkg load image;
+
+
+fruit = imread('C:\Users\Zchemisc\Documents\MATLAB\fruits.png');
+imageinf = imfinfo ('fruits.png')
+figure(1),imshow(fruit);
+
+resz = imresize(fruit,0.5);
+imwrite(resz,'C:\Users\Zchemisc\Documents\MATLAB\fruits2.png' )
+figure(2),imshow(resz);
+
+convrt = rgb2hsv(fruit);
+imwrite(convrt,'C:\Users\Zchemisc\Documents\MATLAB\fruits3.png' )
+figure(3), imshow(convrt);
+
+
+
+
